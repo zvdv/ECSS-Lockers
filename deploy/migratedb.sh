46510b0b3228eefbcf2f7ad853d87eb7d7ad0655
Hal <haln_01@proton.me>
2024-05-07 15:36:39 -0700

    chore: deploy directory

--- deploy/migratedb.sh
+++ deploy/migratedb.sh
@@ -1,3 +1,8 @@
+#!/bin/bash env
+
+
+container_name="ecss-locker-db"
+
 echo "Warning, this will drop all tables from the database before migrating, you WILL lose all your data."
 read -p "Continue to migrate database? [Y/n] " choice
-[[ $choice == "y" || $choice == "Y" ]] && docker exec ecss-locker-db './home/push-schema.sh'
+[[ $choice == "y" || $choice == "Y" ]] && docker exec $container_name '/home/push-schema.sh'
--- deploy/push-schema.sh
+++ deploy/push-schema.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# This script is meant to be running inside the database container 
+# specified in the docker-compose.yml file.
 
 DB_ARGS=$(echo $DATABASE_URL | awk -F"[:/@?]" '{print "-h " $6 " -u " $4 " -p" $5}' | xargs)
 DATABASE='locker-registration'

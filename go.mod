8c38a23cbc17846cae381654b4f193bbdcac83b7
Hal <haln_01@proton.me>
2024-08-14 23:46:09 -0700

    feat(auth)[wip]: auth flow implemented - Validate and send mail login link

--- go.mod
+++ go.mod
@@ -5,15 +5,18 @@ go 1.23.0
 require (
 	github.com/go-chi/chi/v5 v5.1.0
 	github.com/go-playground/validator/v10 v10.22.0
+	golang.org/x/crypto v0.26.0
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 )
 
 require (
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
-	golang.org/x/crypto v0.26.0 // indirect
 	golang.org/x/net v0.21.0 // indirect
 	golang.org/x/sys v0.23.0 // indirect
 	golang.org/x/text v0.17.0 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 )
--- internal/router/login.go
+++ internal/router/login.go
@@ -1,11 +1,15 @@
 package router
 
 import (
+	"encoding/binary"
 	"fmt"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/go-playground/validator/v10"
+	"github.com/zvdv/ECSS-Lockers/internal"
+	"github.com/zvdv/ECSS-Lockers/internal/crypto"
 )
 
 func uvicEmailValidator(email string) bool {
@@ -29,18 +33,44 @@ func (router *App) login(w http.ResponseWriter, r *http.Request) {
 	// validate email
 	email := r.FormValue("email")
 	if !uvicEmailValidator(email) {
-		w.Write([]byte(`
+		_, err := w.Write([]byte(`
             <button type="submit" class="btn btn-primary btn-block">Login</button>
             <div class="form-error">Invalid UVic email address</div>
             `))
+		if err != nil {
+			panic(err)
+		}
 		return
 	}
 
 	// TODO: send email
+	if err := sendLoginLink(email); err != nil {
+		panic(err)
+	}
 
 	// response
 	html := fmt.Sprintf(`<span class="form-info">
-        Login link sent to %s!<br/>
+        Login link sent to %s!
         </span>`, email)
-	w.Write([]byte(html))
+	if _, err := w.Write([]byte(html)); err != nil {
+		panic(err)
+	}
+}
+
+func sendLoginLink(email string) error {
+	// block schema: 8 byte timestamp, seconds in UTC, expires in 15 mins
+	// (900 seconds) this check is done on the receiving end.
+	// + len(email)
+	buf := make([]byte, len(email)+8)
+	ts := time.Now().Unix()
+	binary.BigEndian.AppendUint64(buf[:8], uint64(ts))
+	copy(buf[8:], email)
+	var key [32]byte
+
+	ciphertext, err := crypto.Encrypt(key[:], buf, nil)
+	if err != nil {
+		return err
+	}
+	token := crypto.Base64Encode.EncodeToString(ciphertext)
+	return internal.SendMail(email, token)
 }

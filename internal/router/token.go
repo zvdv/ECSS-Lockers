9d720b49e0320e205d39132e72c6004ecd01f36d
Hal <haln_01@proton.me>
2024-08-15 20:20:50 -0700

    feat(auth): token parser

--- internal/router/token.go
+++ internal/router/token.go
@@ -34,4 +34,34 @@ func (router *App) apiTokenValidator(w http.ResponseWriter, r *http.Request) {
 	logger.Trace("token:%s", token)
 
 	w.Header().Add("HX-Redirect", "/dash")
+
+func makeTokenFromEmail(email string) (string, error) {
+	// block schema: 8 byte timestamp, seconds in UTC, expires in 15 mins
+	// (900 seconds) this check is done on the receiving end.
+	// + len(email)
+	buf := make([]byte, len(email)+8)
+	binary.BigEndian.PutUint64(buf[:8], uint64(time.Now().Unix()))
+	copy(buf[8:], email)
+
+	ciphertext, err := crypto.Encrypt(internal.Env.CipherKey, buf, nil)
+	if err != nil {
+		return "", err
+	}
+
+	return hex.EncodeToString(ciphertext), nil
+}
+
+// returns email, token created time, error
+func parseToken(token string) (string, uint64, error) {
+	decodedTokenBytes, err := hex.DecodeString(token)
+	if err != nil {
+		return "", 0, err
+	}
+
+	pt, err := crypto.Decrypt(internal.Env.CipherKey, decodedTokenBytes, nil)
+	if err != nil {
+		return "", 0, err
+	}
+
+	return string(pt[8:]), binary.BigEndian.Uint64(pt[:8]), nil
 }
---
+++ internal/router/token_test.go
@@ -0,0 +1,31 @@
+package router
+
+import (
+	"testing"
+
+	"github.com/zvdv/ECSS-Lockers/internal"
+)
+
+const testCipherKey string = "qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq"
+
+func TestTokenMaker(t *testing.T) {
+	if len(testCipherKey) != 32 {
+		panic(len(testCipherKey))
+	}
+	internal.Env.CipherKey = []byte(testCipherKey)
+
+	email := "halnguyen@uvic.ca"
+	tok, err := makeTokenFromEmail(email)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	email_, _, err := parseToken(tok)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if email != email_ {
+		t.Fail()
+	}
+}

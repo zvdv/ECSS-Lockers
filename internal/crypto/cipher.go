87eba34bac59b84e83736ffde2a5062b59ad9cb4
Hal <haln_01@proton.me>
2024-08-15 20:20:07 -0700

    fix: removed base64 encoding, not url safe

--- internal/crypto/cipher.go
+++ internal/crypto/cipher.go
@@ -2,13 +2,8 @@ package crypto
 
 import (
 	"crypto/rand"
-	"encoding/base64"
 
 	"golang.org/x/crypto/chacha20poly1305"
-)
-
-var (
-	Base64Encode = base64.RawStdEncoding
 )
 
 // key length 32 bytes
